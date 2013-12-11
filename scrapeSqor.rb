ecbdac2e55861ce766b439396af6b32e01094ce1
Jason <jason.bautista@sqor.com>
2013-12-11 15:10:03 -0800

    Fixed bug with comma and added player count..

--- scrapeSqor.rb
+++ scrapeSqor.rb
@@ -9,24 +9,27 @@ require "json"
  $max = 5
  # max = 22
 
+ $playerCount = 0
  begin
      File.open("nba.json", 'w') { |file| 
 
-        file.write(" { rows: [")
+        file.write(' { "rows": [')
         while $ii < $max do
-            if $ii != 0 
-                file.write(",")
-            end
-
             response = Net::HTTP.get_response("cms-dev.sqor.com","/rest/sports/players/?sport=nba&offset=" + currentOffset.to_s)
             currentOffset = currentOffset + 25;
             arrayName = "offsets['" + currentOffset.to_s + "'] = ";
             # puts response.body
 
-            #json = response.body
+            json = response.body
             parsed = JSON.parse(json)
 
             parsed['rows'].each do  |row|
+                if $playerCount != 0 
+                    file.write(",")
+                end
+                $playerCount = $playerCount + 1
+
+
                 # p row.to_json
                 file.write(row.to_json)
             end
@@ -36,6 +39,7 @@ require "json"
         end
         file.write(" ]} ")
      }
+     puts $playerCount
 
  rescue
      p 'error'
