d2eaed59b05edf6725c9f832d68ffefd4b90c807
Jason <jason.bautista@sqor.com>
2013-12-11 14:55:42 -0800

    parsing our rest api

--- scrapeSqor.rb
+++ scrapeSqor.rb
@@ -1,23 +1,39 @@
 require 'net/http'
 
+require "json"
 
 
 
  currentOffset = 0;
  $ii = 0;
- $max = 23
+ $max = 5
 
  File.open("nba.json", 'w') { |file| 
+
+    file.write(" { rows: [")
     while $ii < $max do
+        if $ii != 0 
+            file.write(",")
+        end
+
         response = Net::HTTP.get_response("cms-dev.sqor.com","/rest/sports/players/?sport=nba&offset=" + currentOffset.to_s)
         currentOffset = currentOffset + 25;
         arrayName = "offsets['" + currentOffset.to_s + "'] = ";
-        file.write(arrayName)
-        file.write(response.body)
-        file.write(";")
-        puts response.body
+        # puts response.body
+
+        json = response.body
+        parsed = JSON.parse(json)
+
+        parsed['rows'].each do  |row|
+            p row.to_json
+            file.write(row.to_json)
+            break
+        end
+
         $ii = $ii +  1
+
     end
+    file.write(" ]} ")
  }
 
 
